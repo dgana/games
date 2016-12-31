184b311e903366f6855a086a9f4f892a89d5564f
fadlykayo <f_kayo@yahoo.com>
2016-12-31 18:13:34 +0700

    Editing Opening 31/12/16

--- js/intro.js
+++ js/intro.js
@@ -1,1 +1,22 @@
 /* Intro Javascript */
+
+let intro = document.getElementById("intro");
+let battle = document.getElementById("battle");
+let map = document.getElementById("map");
+
+// Init none
+battle.style.display = "none";
+map.style.display = "none";
+
+// Buttons
+var startBtn = document.createElement("img");
+intro.appendChild(startBtn);
+startBtn.setAttribute("id", "start");
+startBtn.setAttribute("class", "button");
+startBtn.style.opacity = 0;
+
+var creditBtn = document.createElement("img");
+intro.appendChild(creditBtn);
+creditBtn.setAttribute("id", "credit");
+creditBtn.setAttribute("class", "button");
+creditBtn.style.opacity = 0;
