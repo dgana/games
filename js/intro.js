6eff45620bb3fd4c89ff5eceda253ce00be18787
fadlykayo <f_kayo@yahoo.com>
2017-01-01 18:27:00 +0700

    Editing Opening 01/01/16

--- js/intro.js
+++ js/intro.js
@@ -14,16 +14,16 @@ map_i.style.display = "none";
 var startBtn = document.createElement("img");
 startBtn.setAttribute("id", "start");
 startBtn.setAttribute("class", "button");
-startBtn.setAttribute("src", "asset/img/opening/c_start.png");
+startBtn.setAttribute("src", "asset/img/opening/c_start_plank.png");
 intro_i.appendChild(startBtn);
-//startBtn.style.opacity = 0;
+startBtn.style.opacity = 0;
 
 var creditBtn = document.createElement("img");
 creditBtn.setAttribute("id", "credit");
 creditBtn.setAttribute("class", "button");
-creditBtn.setAttribute("src", "asset/img/opening/c_credit.png");
+creditBtn.setAttribute("src", "asset/img/opening/c_credit_plank.png");
 intro_i.appendChild(creditBtn);
-//creditBtn.style.opacity = 0;
+creditBtn.style.opacity = 0;
 
 var midPanel = document.createElement("div");
 midPanel.setAttribute("id", "mid-panel");
@@ -32,13 +32,18 @@ var spanMidPanel = document.createElement("span");
 spanMidPanel.setAttribute("id", "mid-panel-text");
 spanMidPanel.setAttribute("class", "text");
 var maleNinjaImg = document.createElement("img");
-maleNinjaImg.setAttribute("src", "asset/img/character/gif/ninja_male_run.gif");
-maleNinjaImg.setAttribute("id", "maleNinja");
+maleNinjaImg.setAttribute("src", "asset/img/opening/ninja_male_run.gif");
+maleNinjaImg.setAttribute("id", "male-ninja");
+maleNinjaImg.setAttribute("class", "button");
 spanMidPanel.appendChild(maleNinjaImg);
+maleNinjaImg.style.opacity = 0;
 var femaleNinjaImg = document.createElement("img");
 femaleNinjaImg.setAttribute("src", "asset/img/opening/n_f (9).gif");
-femaleNinjaImg.setAttribute("id", "femaleNinja");
+femaleNinjaImg.setAttribute("id", "female-ninja");
+femaleNinjaImg.setAttribute("class", "button");
 spanMidPanel.appendChild(femaleNinjaImg);
+femaleNinjaImg.style.opacity = 0;
+
 midPanel.appendChild(spanMidPanel);
 intro.appendChild(midPanel);
 midPanel.style.display = "none";
@@ -58,8 +63,9 @@ creditBtn.addEventListener("click", function () {
   var backBtn = document.createElement("img");
   backBtn.setAttribute("id", "back");
   backBtn.setAttribute("class", "button");
-  backBtn.setAttribute("src", "asset/img/opening/c_back.png");
+  backBtn.setAttribute("src", "asset/img/opening/c_back_plank.png");
   intro_i.appendChild(backBtn);
+  backBtn.style.opacity = 0;
 
   backBtn.addEventListener("click", function () {
     intro_i.setAttribute("class", "general-background opening");
